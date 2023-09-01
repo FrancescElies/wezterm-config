25741196df65dcf2d2109662dc3649ee80c11795
Francesc Elies <francesc.elies@mbbm-ast.com>
2023-09-01 06:43:28 +0200

    neovim integration

--- wezterm.lua
+++ wezterm.lua
@@ -4,6 +4,8 @@
 
 -- NOTE: environment variable WEZTERM_CONFIG_DIR should point to this file
 local w = require 'wezterm'
+local act = require('wezterm').action
+local mux = require('wezterm').mux
 
 local home = os.getenv 'HOME'
 
@@ -14,23 +16,25 @@ local platform = {
 }
 
 local config = {
-  debug_key_events = true,
+  debug_key_events = false,
 }
 
 if platform.is_win then
+  w.log_info 'on windows'
   config.default_prog = { 'nu' }
   config.launch_menu = {
-    { label = 'PowerShell Core',    args = { 'pwsh' } },
+    { label = 'PowerShell Core', args = { 'pwsh' } },
     { label = 'PowerShell Desktop', args = { 'powershell' } },
-    { label = 'Command Prompt',     args = { 'cmd' } },
-    { label = 'Nushell',            args = { 'nu' } },
+    { label = 'Command Prompt', args = { 'cmd' } },
+    { label = 'Nushell', args = { 'nu' } },
   }
 elseif platform.is_mac then
+  w.log_info 'on mac'
   config.default_prog = { home .. '/.cargo/bin/nu' }
   config.launch_menu = {
-    { label = 'Bash',    args = { 'bash' } },
+    { label = 'Bash', args = { 'bash' } },
     { label = 'Nushell', args = { '~/.cargo/bin/nu' } },
-    { label = 'Zsh',     args = { 'zsh' } },
+    { label = 'Zsh', args = { 'zsh' } },
   }
 end
 
@@ -47,54 +51,55 @@ if platform.is_mac then
   mod.alt_ctrl = 'SUPER|CTRL'
 end
 
--- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
-local function is_vim(pane)
-  -- this is set by the plugin, and unset on ExitPre in Neovim
-  return pane:get_user_vars().IS_NVIM == 'true'
+local mod_window = 'CTRL'
+local wez_nvim_action = function(window, pane, action_wez, forward_key_nvim)
+  local current_process = mux.get_window(window:window_id()):active_pane():get_foreground_process_name()
+  w.log_info(current_process)
+
+  if string.find(current_process, 'nvim') then
+    w.log_info 'change window nvim'
+    window:perform_action(forward_key_nvim, pane)
+  else
+    w.log_info 'change window wezterm'
+    window:perform_action(action_wez, pane)
+  end
 end
 
-local direction_keys = {
-  Left = 'h',
-  Down = 'j',
-  Up = 'k',
-  Right = 'l',
-  -- reverse lookup
-  h = 'Left',
-  j = 'Down',
-  k = 'Up',
-  l = 'Right',
-}
+w.on('move-left', function(window, pane)
+  wez_nvim_action(
+    window,
+    pane,
+    act.ActivatePaneDirection 'Left', -- this will execute when the active pane is not a nvim instance
+    act.SendKey { key = 'h', mods = mod_window } -- this key combination will be forwarded to nvim if the active pane is a nvim instance
+  )
+end)
 
-local function split_nav(resize_or_move, key)
-  w.log_info(resize_or_move .. ' ' .. key)
-  return {
-    key = key,
-    mods = resize_or_move == 'resize' and mod.alt or mod.ctrl,
-    action = w.action_callback(function(win, pane)
-      if is_vim(pane) then
-        -- pass the keys through to vim/nvim
-        win:perform_action({
-          SendKey = { key = key, mods = resize_or_move == 'resize' and mod.alt or mod.ctrl },
-        }, pane)
-      else
-        if resize_or_move == 'resize' then
-          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-        else
-          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-        end
-      end
-    end),
-  }
-end
+w.on('move-right', function(window, pane)
+  wez_nvim_action(window, pane, act.ActivatePaneDirection 'Right', act.SendKey { key = 'l', mods = mod_window })
+end)
+
+w.on('move-down', function(window, pane)
+  wez_nvim_action(window, pane, act.ActivatePaneDirection 'Down', act.SendKey { key = 'j', mods = mod_window })
+end)
+
+w.on('move-up', function(window, pane)
+  wez_nvim_action(window, pane, act.ActivatePaneDirection 'Up', act.SendKey { key = 'k', mods = mod_window })
+end)
+
+-- you can add other actions, this unifies the way in which panes and windows are closed
+-- (you'll need to bind <A-x> -> <C-w>q)
+w.on('close-pane', function(window, pane)
+  wez_nvim_action(window, pane, act.CloseCurrentPane { confirm = false }, act.SendKey { key = 'x', mods = 'ALT' })
+end)
 
 config.keys = {
-  { key = 'z',   mods = mod.shift_ctrl, action = w.action.TogglePaneZoomState },
-  { key = ' ',   mods = mod.ctrl,       action = 'DisableDefaultAssignment' },
+  { key = 'z', mods = mod.shift_ctrl, action = w.action.TogglePaneZoomState },
+  { key = ' ', mods = mod.ctrl, action = 'DisableDefaultAssignment' },
   -- fix ctrl-space not reaching the term https://github.com/wez/wezterm/issues/4055#issuecomment-1694542317
-  { key = ' ',   mods = mod.ctrl,       action = w.action.SendKey { key = ' ', mods = mod.ctrl } },
-  { key = 'F1',  mods = 'NONE',         action = w.action.ActivateCopyMode },
-  { key = 'F12', mods = 'NONE',         action = w.action.ShowDebugOverlay },
-  { key = 'a',   mods = mod.alt,        action = w.action.ShowLauncher },
+  { key = ' ', mods = mod.ctrl, action = w.action.SendKey { key = ' ', mods = mod.ctrl } },
+  { key = 'F1', mods = 'NONE', action = w.action.ActivateCopyMode },
+  { key = 'F12', mods = 'NONE', action = w.action.ShowDebugOverlay },
+  { key = 'a', mods = mod.alt, action = w.action.ShowLauncher },
   {
     key = '-',
     mods = mod.alt,
@@ -109,21 +114,17 @@ config.keys = {
   },
 
   { key = 'Enter', mods = mod.alt, action = w.action.DisableDefaultAssignment }, -- broot uses alt-enter
-  { key = 's',     mods = mod.alt, action = w.action.PaneSelect { alphabet = '1234567890' } },
-  { key = 'r',     mods = mod.alt, action = w.action 'ReloadConfiguration' },
-  { key = 'q',     mods = mod.alt, action = w.action { CloseCurrentPane = { confirm = true } } },
-  { key = 'x',     mods = mod.alt, action = w.action { CloseCurrentPane = { confirm = true } } },
 
-  -- move between split panes
-  split_nav('move', 'h'),
-  split_nav('move', 'j'),
-  split_nav('move', 'k'),
-  split_nav('move', 'l'),
-  -- resize panes
-  split_nav('resize', 'h'),
-  split_nav('resize', 'j'),
-  split_nav('resize', 'k'),
-  split_nav('resize', 'l'),
+  { key = 's', mods = mod.alt, action = w.action.PaneSelect { alphabet = '1234567890' } },
+  { key = 'r', mods = mod.alt, action = w.action 'ReloadConfiguration' },
+  { key = 'q', mods = mod.alt, action = w.action { CloseCurrentPane = { confirm = true } } },
+
+  -- window movements
+  { key = 'h', mods = mod_window, action = w.action { EmitEvent = 'move-left' } },
+  { key = 'l', mods = mod_window, action = w.action { EmitEvent = 'move-right' } },
+  { key = 'j', mods = mod_window, action = w.action { EmitEvent = 'move-down' } },
+  { key = 'k', mods = mod_window, action = w.action { EmitEvent = 'move-up' } },
+  { key = 'x', mods = mod.alt, action = w.action { EmitEvent = 'close-pane' } },
 }
 
 config.switch_to_last_active_tab_when_closing_tab = true
