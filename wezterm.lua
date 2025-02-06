c2dfe551da1ed33dda655d5cb7e385003f771139
Francesc Elies <elies@posteo.net>
2025-02-06 08:52:49 +0100

    .

--- wezterm.lua
+++ wezterm.lua
@@ -203,6 +203,7 @@ config.keys = {
                   name = id,
                   spawn = {
                     cwd = label,
+                    -- args = { 'nu', '-e', 'br' }, -- opens broot directly
                     args = { 'nu' }, -- just open shell
                   },
                 },
